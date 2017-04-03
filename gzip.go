96217cc93bb9f8bcf4073b94dd1fd23efe46b151
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-04-03 22:59:43 +0930

    Add panic in writer pool New, not ignore error

--- gzip.go
+++ gzip.go
@@ -36,10 +36,11 @@ func poolIndex(level int) int {
 func addLevelPool(level int) {
 	gzipWriterPools[poolIndex(level)] = &sync.Pool{
 		New: func() interface{} {
-			// NewWriterLevel only returns error on a bad level, we are guaranteeing
-			// that this will be a valid level so it is okay to ignore the returned
-			// error.
-			w, _ := gzip.NewWriterLevel(nil, level)
+			w, err := gzip.NewWriterLevel(nil, level)
+			if err != nil {
+				panic(err)
+			}
+
 			return w
 		},
 	}
