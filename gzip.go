andreasunger/gziphandler

commit d8a44fd7a9598d22c88757b93fb9a76b7caf2562

Remove `buf: []byte{}` in GzipWithLevelAndMinSize

gzip.go
@@ -255,8 +255,6 @@ func GzipWithLevelAndMinSize(h http.Handler, level, minSize int) (http.Handler, 
 				ResponseWriter: w,
 				index:          index,
 				minSize:        minSize,
-
-				buf: []byte{},
 			}
 			defer gw.Close()
 

commit 6c46a54bca8dd16fe5b2a412d7e149e5aa901757

Fix small responses (broken by d8a44fd7a9)

gzip.go
@@ -87,7 +87,8 @@ func (w *responseWriter) startGzip() error {
 	w.gw = w.pool.Get().(*gzip.Writer)
 	w.gw.Reset(w.ResponseWriter)
 
-	if w.buf == nil {
+	if len(w.buf) == 0 {
+		w.buf = nil
 		return nil
 	}
 
@@ -180,6 +181,8 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		code: http.StatusOK,
 
 		minSize: h.minSize,
+
+		buf: []byte{},
 	}
 	defer gw.Close()
 